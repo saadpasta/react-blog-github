1d8e9e58a4954263c4e631b51a6cff2ff0d56042
Saad Pasta <saadpasta70@gmail.com>
2020-02-03 13:07:10 +0500

    add : routing for blog

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
 import App from "./App"
 import Blogs from "./Containers/Blogs/Blogs"
+import BlogHome from "./Components/BlogHome/BlogHome";
 
 var createBrowserHistory = require("history").createBrowserHistory;
 const history = createBrowserHistory();
@@ -12,6 +13,7 @@ const Routes = () => {
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={Blogs} /> 
+        <Route exact path="/blog/:title/:issueNumber" component={BlogHome}/>
       </Switch>
     </Router>
   );
