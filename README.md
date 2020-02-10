8d0e719522961f6777a749a060b5862dd2afb4ed
Saad Pasta <saadpasta70@gmail.com>
2020-02-10 12:31:12 +0500

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# React Github Issue Blog
-### Blog Built on Reactjs Using Github Issues as the Data Source . 
-
-
-⚠️ In Progress
---
+++ precache-manifest.a7bd2ca3ab16708eeff48768f2ce731c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57c9946b841898de9b075dd8503101b",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "4be7a0cdc29ff305b0cf",
+    "url": "/blog/static/css/main.80073573.chunk.css"
+  },
+  {
+    "revision": "9ecd37084e67803b763d",
+    "url": "/blog/static/js/2.cdb593de.chunk.js"
+  },
+  {
+    "revision": "b71e68075829df524d9270ea31bf21c0",
+    "url": "/blog/static/js/2.cdb593de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4be7a0cdc29ff305b0cf",
+    "url": "/blog/static/js/main.ba67ca72.chunk.js"
+  },
+  {
+    "revision": "612f0132a788c8b22ad8",
+    "url": "/blog/static/js/runtime-main.41e0433e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
