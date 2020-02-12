ebfba5c81439a329513789fac22645c925a46260
Saad Pasta <saadpasta70@gmail.com>
2020-02-12 13:15:59 +0500

    Updates

---
+++ precache-manifest.fff25b34deb99cbfe0ecf18509b3c0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d37f72d92a45306332770508b22041",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "ca8c31d52cc7f5a88b6d",
+    "url": "/react-blog-github/static/css/main.9362a01a.chunk.css"
+  },
+  {
+    "revision": "f01d783b37fe1d169bcf",
+    "url": "/react-blog-github/static/js/2.04749bfa.chunk.js"
+  },
+  {
+    "revision": "b71e68075829df524d9270ea31bf21c0",
+    "url": "/react-blog-github/static/js/2.04749bfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca8c31d52cc7f5a88b6d",
+    "url": "/react-blog-github/static/js/main.b0679e21.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-blog-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
