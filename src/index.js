412be363f3622631ac861ee8f74f96061c5f70c9
Slim Coder <sharma_vivek62@yahoo.com>
2020-02-24 16:18:59 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,4 +10,6 @@ ReactDOM.render(<Routes />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if(process.env.NODE_ENV === 'production') serviceWorker.register();
+else serviceWorker.unregister();
+
