b4515309e3ea3ef831afc27580a07baa397176aa
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-06-02 21:04:04 +0530

    Removed unnecessary console.logs and comments

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,4 @@
-import React, {useContext} from 'react'
-import { ThemeContext } from "styled-components";
-// import Toggle from "../../Components/Theme/Toggler";
+import React from 'react'
 
 import {
     HeaderContainer,
@@ -11,13 +9,10 @@ import {
 } from './'
 import { config } from "../../config"
 
-export const Header = ({theme, toggleTheme}) => {
-    const themeContext = useContext(ThemeContext)
-    console.log(themeContext)
+export const Header = () => {
     return (
         <HeaderContainer>
             <GithubLogin isAbsolute={true} />
-            {/* <Toggle theme={theme.mode} toggleTheme={toggleTheme} /> */}
             <HeaderWrapper>
                 <HeaderTitle>{config.title}</HeaderTitle>
                 <HeaderSubtitle>{config.subtitle}</HeaderSubtitle>
