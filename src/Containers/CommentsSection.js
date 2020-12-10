2be6aaa898ae120883e3e524320db68916264ffa
Erica Gerez <ericagerez63@gmail.com>
2020-12-09 21:31:09 -0300

    Pagination for articles

--- src/Containers/CommentsSection.js
+++ src/Containers/CommentsSection.js
@@ -6,17 +6,42 @@ import {
   CommentLinkContainer,
 } from "../Components/Post/Comment";
 
+import Pagination from '@material-ui/lab/Pagination';
+
+import { useState } from "react";
+
 const CommentsSection = ({postUrl, comments}) => {
+  
+  const itemsPerPage = 3;
+  const noOfPages = Math.ceil(comments.length / 5);
+  const [page, setPage] = useState(1);
+
+  const handleChange = (event, value) => {
+      setPage(value);
+  }; 
+
   return (
     <>
       <CommentLinkContainer>
         <CommentLink href={postUrl + "#issue-comment-box"} target="_blank">
-          Post a comment
+          Post a comment  
         </CommentLink>
       </CommentLinkContainer>
-      {comments.map((v, id) => (
+      {comments.slice((page - 1) * itemsPerPage, page * itemsPerPage).map((v, id) => (
         <Comment comment={v} key={id} />
       ))}
+      
+       <Pagination
+          count={noOfPages}
+          color= "primary"
+          variant="outlined"
+          page={page}
+          onChange={handleChange}
+          defaultPage={1}
+          size="small"
+          showFirstButton
+          showLastButton
+      />
     </>
   );
 };
