824990bcb154a15620eeb1fb3a89f2a96001cfa4
hydtechbloginc <81985743+hydtechbloginc@users.noreply.github.com>
2021-04-05 23:39:14 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "Z2hwX3NVZEpETmRET3huTWRUWGRoWDhhblpNNEoxb1FqQjBvem1GNg==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "hydtechbloginc",
 
   // Your Github Repo Name Where You Have your issues as Blog
   githubRepo: "react-blog-github",
@@ -11,14 +11,14 @@ export const config = {
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: 'Iv1.9dda76533c313deb',
+  OAuthSecret: '02584638e2965d1eec5995c7d73de334ecf6b973',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "hyd tech blog" ,
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle : "blog for fun",
 
   // Header customization
   header: {
