fb8d04b798a134921500ec1817b3a7bb18933ab7
Saad Pasta <saadpasta70@gmail.com>
2021-10-09 12:55:18 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "Z2hwX0xhcG9DOW9qSktOVTlNUHJUdjVuM3dvTzBoYlZlTTJNWU5VYw==",
 
   // Your Github UserName
   githubUserName: "saadpasta",
