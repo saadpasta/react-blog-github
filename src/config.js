be4e3825359599e7cd357a6d95b0e69a84faf6c7
Zinstjerna <jltaoshirley@gmail.com>
2021-12-14 14:28:30 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "Z2hwX0xhcG9DOW9qSktOVTlNUHJUdjVuM3dvTzBoYlZlTTJNWU5VYw==",
+  githubConvertedToken: "Z2hwX0ZUSVlQT1RJSGZMOGNBS09WWndVaDNvZFVVWlZ5ZjNIUXZDWg==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "Uraniluib",
 
   // Your Github Repo Name Where You Have your issues as Blog
   githubRepo: "react-blog-github",
@@ -11,14 +11,14 @@ export const config = {
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: 'Iv1.784b2883d190d053',
+  OAuthSecret: '7329d9f913e33b400bee3420b53d58932b06acc7',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "空泛传说" ,
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle : "异界大陆小说设定集",
 
   // Header customization
   header: {
